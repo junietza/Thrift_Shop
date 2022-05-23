c36b95aaf5956ba4df281a2bba458f1124299d8f
mcaceres <mcaceres@inss.gob.ni>
2022-05-23 11:39:36 -0600

    carrousel y menu responsive

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@ const despliegaMenu = () => {
     document.querySelector(".menu").classList.toggle('visible')
 }
 document.getElementById('btn-burguer').addEventListener('click', despliegaMenu)
+
+
+// CARROUSEL
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+} 
