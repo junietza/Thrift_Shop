99f4d356834086d6f7fbb11d10eb87130d450cd1
junietza <lopezguerrero2503@gmail.com>
2022-07-05 22:57:49 -0600

    Finalizado Oferta

--- js/oferta.js
+++ js/oferta.js
@@ -29,3 +29,35 @@ imagenes.forEach(img => {
                 imagenMostrada.setAttribute("src", rutaImagen)
         })
 })
+
+const getRemainTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+
+    return { remainTime, remainSeconds, remainMinutes, remainHours, remainDays };
+};
+
+const countdown = (deadline, elem, finalMessage) => {
+
+    const el = document.getElementById(elem);
+
+    const timerUpdate = setInterval(() => {
+
+        let t = getRemainTime(deadline);
+
+        el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+
+        if (t.remainTime <= 1) {
+            clearInterval(timerUpdate);
+            el.innerHTML = finalMessage;
+        }
+
+    }, 1000);
+
+};
+
+countdown('Jul 06 2022 17:00:00 GMT-0600', 'clock', 'Feliz Fin Semestre')
